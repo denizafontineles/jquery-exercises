f6b9a129b20c870a2662f8ab4548f33a58cb4c9f
denizafontineles <denizafontineles@outlook.com>
2019-10-15 17:04:39 -0300

    Insere card pelo jquery

--- Efeitos/js/page3.js
+++ Efeitos/js/page3.js
@@ -1,10 +1,15 @@
 // **3.** No arquivo page3.html, no clique do botão programe toogle para cada card, com tempos diferentes para cada um.
 
 $(function() {
-    $(".btn-primary").click(function() {
-        $(".card-one").toggle(5000)
-        $(".card-two").toggle(3000)
-        $(".card-three").toggle(1000)
-    })
+        $(".btn-primary").click(function() {
+            $(".card-one").toggle(5000)
+            $(".card-two").toggle(3000)
+            $(".card-three").toggle(1000)
+        })
 
-})
+        $(".btn-seg").click(function() {
+            let content = $(".row").html()
+            $(content).append(".row")
+        })
+    })
+    // Na página 3, crie um segundo botão que insira mais um fileira de cards.
